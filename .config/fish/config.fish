0e970f91df3453c9d88e3d68426fae2d0fd74940
august-norkko <boundlessvolatilecrow@gmail.com>
2024-08-11 16:38:49 +0200

    dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,3 +3,18 @@ set fish_greeting
 if status is-interactive
 
 end
+
+alias gs="git status"
+alias ga="git add"
+alias gl="git log"
+alias gp="git push"
+alias gr="git remote"
+alias gb="git branch"
+alias gco="git checkout"
+alias gc="git commit -m"
+alias gd="git diff"
+alias gm="git merge"
+alias grb="git rebase"
+alias dotfiles="cd $HOME/dotfiles;echo '$(pwd)';stow .;cd"
+alias jctl="journalctl -f"
+alias ls="ls -a"
